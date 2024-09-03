4bacb4b486d781afcacaf23f3419173c5ea87d58
Matheus Silva <mathsant_99@hotmail.com>
2024-09-03 14:57:00 -0300

    fix: docker compose

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CashFlow.Api.Controllers;
@@ -11,8 +12,29 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestExpenseJson request)
     {
-        var response = new RegisterExpenseUseCase().Execute(request);
+        try
+        {
+            var response = new RegisterExpenseUseCase().Execute(request);
 
-        return Created(string.Empty, response);
+            return Created(string.Empty, response);
+        }
+        catch (ArgumentException ex)
+        {
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = ex.Message
+            };
+
+            return BadRequest(errorResponse);
+        }
+        catch
+        {
+            var errorResponse = new ResponseErrorJson
+            {
+                ErrorMessage = "Unknown error."
+            };
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+        }
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
@@ -6,8 +7,34 @@ public class RegisterExpenseUseCase
 {
     public ResponseRegisterExpenseJson Execute(RequestExpenseJson expense)
     {
-        // To-Do: Validations
+        Validate(expense);
 
         return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title.Trim());
+        if (titleIsEmpty)
+        {
+            throw new ArgumentException("Title is required");
+        }
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The amount should be biggiest of zero");
+        }
+
+        var dateValidation = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (dateValidation > 0)
+        {
+            throw new ArgumentException("Invalid date");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentTypeEnum), request.PaymentType);
+        if (paymentTypeIsValid == false)
+        {
+            throw new ArgumentException("Payment type is not valid");
+        }
     }
 }
